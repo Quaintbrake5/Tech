258296c1c80cabcebdd897110cd67952d849ad78
Quaintbrake5 <denzylibe6@gmail.com>
2024-09-18 13:46:11 +0100

    Added my project

---
+++ src/Approuter.jsx
@@ -0,0 +1,18 @@
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+
+const Approuter = () => {
+  return (
+    <>
+        <Router>
+            {/* <Navigation/> */}
+            <Routes>
+                <Route path="/">
+                    
+                </Route>
+            </Routes>
+        </Router>
+    </>
+  );
+};
+
+export default Approuter;
