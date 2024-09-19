6551f10f79808cff71abd58a189fd5acd4ae694d
Quaintbrake5 <denzylibe6@gmail.com>
2024-09-19 10:20:59 +0100

    Added my project

--- src/Approuter.jsx
+++ src/Approuter.jsx
@@ -1,11 +1,13 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Navigation from './Components/nav/Navigation'
+import HeroSection from "./Components/Hero/HeroSection";
 
 const Approuter = () => {
   return (
     <>
         <Router>
             <Navigation/>
+            <HeroSection/>
             <Routes>
                 <Route path="/">
                     
